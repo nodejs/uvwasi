ad96844e9e59d264a42ca30fc8bc9212721b7265
cjihrig <cjihrig@gmail.com>
2020-07-25 10:55:51 -0400

    rename DEBUG() macro to UVWASI_DEBUG()
    This commit renames the DEBUG() macro to prevent name
    collisions.
    
    Fixes: https://github.com/cjihrig/uvwasi/issues/142

--- src/debug.h
+++ src/debug.h
@@ -6,10 +6,10 @@
 # define __STDC_FORMAT_MACROS
 #endif
 # include <inttypes.h>
-# define DEBUG(fmt, ...)                                                      \
+# define UVWASI_DEBUG(fmt, ...)                                                      \
     do { fprintf(stderr, fmt, __VA_ARGS__); } while (0)
 #else
-# define DEBUG(fmt, ...)
+# define UVWASI_DEBUG(fmt, ...)
 #endif
 
 #endif /* __UVWASI_DEBUG_H__ */
