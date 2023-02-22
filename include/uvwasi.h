96f37feebb7f106e9ed6fd22ef9146c20a64bb9c
Michael Dawson <mdawson@devrus.com>
2023-02-22 15:30:30 -0500

    add stub for sock_accept (#185)
    * add sub for sock_accept
    
    Refs: https://github.com/nodejs/uvwasi/issues/183
    
    add the stub for sock_accept so that we have
    stubs for all method in the current version of
    snapshot 1. sock_accept was added later after
    snapshot 1 was first documented.
    
    Does not complete #183 but is first step.
    
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

--- include/uvwasi.h
+++ include/uvwasi.h
@@ -251,6 +251,10 @@ uvwasi_errno_t uvwasi_random_get(uvwasi_t* uvwasi,
                                  void* buf,
                                  uvwasi_size_t buf_len);
 uvwasi_errno_t uvwasi_sched_yield(uvwasi_t* uvwasi);
+uvwasi_errno_t uvwasi_sock_accept(uvwasi_t* uvwasi,
+                                  uvwasi_fd_t sock,
+                                  uvwasi_fdflags_t flags,
+                                  uvwasi_fd_t* fd);
 uvwasi_errno_t uvwasi_sock_recv(uvwasi_t* uvwasi,
                                 uvwasi_fd_t sock,
                                 const uvwasi_iovec_t* ri_data,
